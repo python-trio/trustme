3bfee7404607c1bd2d715fd45a96d29ee584893a
Nathaniel J. Smith <njs@pobox.com>
2017-07-17 22:28:38 -0700

    [travis] Fix silly bug in travis script

--- ci/travis.sh
+++ ci/travis.sh
@@ -10,7 +10,7 @@ pip install dist/*.zip
 pip install -Ur test-requirements.txt
 mkdir empty
 pushd empty
-INSTALLDIR=$(python -c "import os, trustme; print(os.path.dirname(trustme))")
+INSTALLDIR=$(python -c "import os, trustme; print(os.path.dirname(trustme.__file__))")
 pytest ../tests --cov="$INSTALLDIR" --cov=../tests --cov-config="../.coveragerc"
 
 pip install codecov
