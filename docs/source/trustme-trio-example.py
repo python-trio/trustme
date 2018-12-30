c255b0e430b2376d9872d3290444986deac9baa6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-30 03:48:02 +0100

    Add an example of the client cert auth

--- docs/source/trustme-trio-example.py
+++ docs/source/trustme-trio-example.py
@@ -1,11 +1,15 @@
 # trustme-trio-example.py
 
+import ssl
+
 import trustme
 import trio
 
 # Create our fake certificates
 ca = trustme.CA()
+client_ca = trustme.CA()
 server_cert = ca.issue_cert(u"test-host.example.org")
+client_cert = client_ca.issue_cert(u"@webknjaz here")
 
 
 async def demo_server(server_raw_stream):
@@ -15,6 +19,13 @@ async def demo_server(server_raw_stream):
     # Set up the server's SSLContext to use our fake server cert
     server_cert.configure_cert(server_ssl_context)
 
+    # Set up the server's SSLContext to trust our fake CA, that signed
+    # our client cert
+    client_ca.configure_trust(server_ssl_context)
+
+    # Verify that client sent us their TLS cert signed by a trusted CA
+    server_ssl_context.verify_mode = ssl.CERT_REQUIRED
+
     server_ssl_stream = trio.ssl.SSLStream(
         server_raw_stream,
         server_ssl_context,
@@ -23,6 +34,8 @@ async def demo_server(server_raw_stream):
 
     # Send some data to check that the connection is really working
     await server_ssl_stream.send_all(b"x")
+    print("Server successfully sent data over the encrypted channel!")
+    print("Client cert looks like:", server_ssl_stream.getpeercert())
 
 
 async def demo_client(client_raw_stream):
@@ -31,6 +44,9 @@ async def demo_client(client_raw_stream):
     # Set up the client's SSLContext to trust our fake CA, that signed our
     # server cert
     ca.configure_trust(client_ssl_context)
+
+    # Set up the client's SSLContext to use our fake client cert
+    client_cert.configure_cert(client_ssl_context)
 
     client_ssl_stream = trio.ssl.SSLStream(
         client_raw_stream,
@@ -42,7 +58,7 @@ async def demo_client(client_raw_stream):
 
     assert await client_ssl_stream.receive_some(1) == b"x"
     print("Client successfully received data over the encrypted channel!")
-    print("Cert looks like:", client_ssl_stream.getpeercert())
+    print("Server cert looks like:", client_ssl_stream.getpeercert())
 
 
 async def main():
