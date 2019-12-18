d14f23ab152c1583b75cf1dbda3b3994c9af511a
Gilbert Gilb's <gilbsgilbert@gmail.com>
2019-12-18 19:58:14 +0100

    Run tests for python 3.8 in CI.

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: System :: Networking",
         "Topic :: Security :: Cryptography",
         "Topic :: Software Development :: Testing",
