4f70e7fe55195f310260bf4c634941e7423d1a4e
Quentin Pradet <quentin.pradet@gmail.com>
2020-12-08 11:10:12 +0400

    Switch from Travis/Appveyor to GitHub Actions

--- ci.sh
+++ ci.sh
@@ -2,21 +2,21 @@
 
 set -exu -o pipefail
 
-pip install -U pip setuptools wheel
+python -m pip install -U pip setuptools wheel
 
 python setup.py sdist --formats=zip
-pip install dist/*.zip
+python -m pip install dist/*.zip
 
 # Actual tests
 
-pip install -Ur test-requirements.txt
+python -m pip install -Ur test-requirements.txt
 if [ -n "${OLD_CRYPTOGRAPHY:-}" ]; then
-    pip install cryptography=="${OLD_CRYPTOGRAPHY}"
+    python -m pip install cryptography=="${OLD_CRYPTOGRAPHY}"
 fi
 mkdir empty
 pushd empty
 INSTALLDIR=$(python -c "import os, trustme; print(os.path.dirname(trustme.__file__))")
 pytest -W error -ra -s ../tests --cov="$INSTALLDIR" --cov=../tests --cov-config="../.coveragerc"
 
-pip install codecov
+python -m pip install codecov
 codecov -F $(uname | tr A-Z a-z)
