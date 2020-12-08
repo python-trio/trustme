6572af3be5a0c64ae8d9ba320e24ea236fcc6135
Quentin Pradet <quentin.pradet@gmail.com>
2020-12-08 11:05:41 +0400

    Remove DOC_BUILD logic from ci/travis.sh
    We now build the documentation using ReadTheDocs

--- ci/travis.sh
+++ ci/travis.sh
@@ -7,24 +7,16 @@ pip install -U pip setuptools wheel
 python setup.py sdist --formats=zip
 pip install dist/*.zip
 
-# ${FOO:-} means "$FOO if defined, else empty string"
-if [ "${DOC_BUILD:-}" = "1" ]; then
-    pip install -U sphinx
-    pip install -U -r ci/rtd-requirements.txt
-    cd docs
-    sphinx-build -nW -b html source build
-else
-    # Actual tests
+# Actual tests
 
-    pip install -Ur test-requirements.txt
-    if [ -n "${OLD_CRYPTOGRAPHY:-}" ]; then
-        pip install cryptography=="${OLD_CRYPTOGRAPHY}"
-    fi
-    mkdir empty
-    pushd empty
-    INSTALLDIR=$(python -c "import os, trustme; print(os.path.dirname(trustme.__file__))")
-    pytest -W error -ra -s ../tests --cov="$INSTALLDIR" --cov=../tests --cov-config="../.coveragerc"
-
-    pip install codecov
-    codecov -F $(uname | tr A-Z a-z)
+pip install -Ur test-requirements.txt
+if [ -n "${OLD_CRYPTOGRAPHY:-}" ]; then
+    pip install cryptography=="${OLD_CRYPTOGRAPHY}"
 fi
+mkdir empty
+pushd empty
+INSTALLDIR=$(python -c "import os, trustme; print(os.path.dirname(trustme.__file__))")
+pytest -W error -ra -s ../tests --cov="$INSTALLDIR" --cov=../tests --cov-config="../.coveragerc"
+
+pip install codecov
+codecov -F $(uname | tr A-Z a-z)
