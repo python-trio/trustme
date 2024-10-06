865f11318824019f29b4cef52f2808c54e524651
Quentin Pradet <quentin.pradet@gmail.com>
2024-10-06 22:07:03 +0400

    Fix lint.sh and ci.sh scripts

--- lint.sh
+++ lint.sh
@@ -4,7 +4,7 @@ set -exu -o pipefail
 
 python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
 
-python -m pip install -U pip setuptools wheel
+python -m pip install -U pip
 python -m pip --version
 
 # Dependencies
